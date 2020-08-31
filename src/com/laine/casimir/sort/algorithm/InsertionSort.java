a921b6f73266012fd6c082ba608249fb3d35bd60
Casimir Laine <casimir.laine@gmail.com>
2020-08-31 23:47:40 +0300

    Refactored InsertionSort.

--- src/com/laine/casimir/sort/algorithm/InsertionSort.java
+++ src/com/laine/casimir/sort/algorithm/InsertionSort.java
@@ -4,15 +4,9 @@ public class InsertionSort extends SortingAlgorithm {
 
     @Override
     protected void onSort() {
-        for (int index = 0; index < length(); index++) {
-            if (index != 0) {
-                for (int subIndex = index; subIndex > 0; subIndex--) {
-                    if (greater(subIndex - 1, subIndex)) {
-                        swap(subIndex, subIndex - 1);
-                    } else {
-                        break;
-                    }
-                }
+        for (int index = 1; index < length(); index++) {
+            for (int subIndex = index; subIndex > 0 && greater(subIndex - 1, subIndex); subIndex--) {
+                swap(subIndex, subIndex - 1);
             }
         }
     }
